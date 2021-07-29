1ded458fa388b3b392c8b502358c87e780edd7be
jyockey <sidereal@gmail.com>
2021-07-29 14:37:52 -0700

    Initial adds

---
+++ src/sandbox/object_tracking.py
@@ -0,0 +1,87 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import time
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", type=str, help="path to input video file")
+ap.add_argument("-t", "--tracker", type=str, default="kcf",
+                help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+OPENCV_OBJECT_TRACKERS = {
+    "csrt": cv2.TrackerCSRT_create,
+    "kcf": cv2.TrackerKCF_create,
+    "mil": cv2.TrackerMIL_create
+}
+
+tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+
+initBB = None
+
+# if a video path was not supplied, grab the reference to the web cam
+vs = cv2.VideoCapture(args["video"])
+
+# initialize the FPS throughput estimator
+fps = None
+
+# loop over frames from the video stream
+while vs.isOpened():
+    ret, frame = vs.read()
+    # resize the frame (so we can process it faster) and grab the
+    # frame dimensions
+    if not ret:
+        break
+
+    frame = imutils.resize(frame, width=500)
+    (H, W) = frame.shape[:2]
+
+    # check to see if we are currently tracking an object
+    if initBB is not None:
+        # grab the new bounding box coordinates of the object
+        (success, box) = tracker.update(frame)
+        # check to see if the tracking was a success
+        if success:
+            (x, y, w, h) = [int(v) for v in box]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # update the FPS counter
+        fps.update()
+        fps.stop()
+        # initialize the set of information we'll be displaying on
+        # the frame
+        info = [
+            ("Tracker", args["tracker"]),
+            ("Success", "Yes" if success else "No"),
+            ("FPS", "{:.2f}".format(fps.fps())),
+        ]
+        # loop over the info tuples and draw them on our frame
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # if the 's' key is selected, we are going to "select" a bounding
+    # box to track
+    if key == ord("s"):
+        # select the bounding box of the object we want to track (make
+        # sure you press ENTER or SPACE after selecting the ROI)
+        initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+                               showCrosshair=True)
+        # start OpenCV object tracker using the supplied bounding box
+        # coordinates, then start the FPS throughput estimator as well
+        tracker.init(frame, initBB)
+        fps = FPS().start()
+    # if the `q` key was pressed, break from the loop
+    elif key == ord("q"):
+        break
+
+vs.release()
+# close all windows
+cv2.destroyAllWindows()
---
+++ src/sandbox/video_play.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('c:\\proj\\footeye\\highlight_vid.mp4')
+while cap.isOpened():
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
