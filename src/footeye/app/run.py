983267e09d3f1a633755eb39207edf680e308905
jyockey <sidereal@gmail.com>
2021-08-02 17:42:11 -0700

    Greatly improve frame logging

--- src/footeye/app/run.py
+++ src/footeye/app/run.py
@@ -1,25 +1,39 @@
-from ..cvlib import frameutils
-from ..cvlib import features
+import footeye.cvlib.frameutils as frameutils
+import footeye.cvlib.features as features
 
 import cv2 as cv
 
 
+features.enable_logging()
 frame = frameutils.extract_frame('c:\\stuff\\portland_la.ts', 75950)
-frame = features.interactive_find_pitch(frame)
-edges = features.find_lines(frame)
-cv.imshow('frame', edges)
-cv.waitKey(0)
 
-vid = cv.VideoCapture('c:\\stuff\\portland_la.ts')
-while vid.isOpened():
-    ret, frame = vid.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    cv.imshow('frame', features.extract_players(frame))
-    if cv.waitKey(1) == ord('q'):
-        break
+features.extract_players(frame)
+idx = 0
+key = 0
+while (key != 113):
+    cv.imshow('frame', features.logframes[idx])
+    key = cv.waitKeyEx(0)
+    if key == 2424832:
+        idx = idx - 1
+    else:
+        idx = (idx + 1) % len(features.logframes)
+
+#pitch = features.interactive_find_pitch(frame)
+#edges = features.find_lines(pitch)
+#cv.imshow('frame', edges)
+#cv.imshow('frame', features.extract_players(frame))
+#cv.waitKey(0)
+
+#vid = cv.VideoCapture('c:\\proj\\footeye\\king_vid.mp4')
+#while vid.isOpened():
+#    ret, frame = vid.read()
+#    # if frame is read correctly ret is True
+#    if not ret:
+#        print("Can't receive frame (stream end?). Exiting ...")
+#        break
+#    cv.imshow('frame', features.extract_players(frame))
+#    if cv.waitKey(1) == ord('q'):
+#        break
 # When everything done, release the capture
-vid.release()
-cv.destroyAllWindows()
+#vid.release()
+#cv.destroyAllWindows()
--- src/footeye/cvlib/features.py
+++ src/footeye/cvlib/features.py
@@ -1,10 +1,29 @@
 import cv2 as cv
 import numpy as np
 
-import frameutils
+import footeye.cvlib.frameutils as frameutils
 
 lower_green = np.array([30, 20, 20])
 upper_green = np.array([90, 180, 220])
+
+logframes = None
+
+
+def enable_logging():
+    global logframes
+    logframes = []
+
+
+def log_frame(frame, desc):
+    global logframes
+    if logframes is not None:
+        if (len(frame.shape) < 3):
+            copy = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+        else:
+            copy = frame.copy()
+        cv.putText(copy, desc, (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.6,
+                   (0, 0, 255), 2)
+        logframes.append(copy)
 
 
 def mask_green(frame):
@@ -18,11 +37,15 @@ def mask_white(frame):
 
 
 def pitch_mask(frame):
-    blurred = cv.medianBlur(frame, 5)
-    mask = mask_green(blurred)
+    frame = cv.medianBlur(frame, 5)
+    log_frame(frame, "Blurred")
+    mask = mask_green(frame)
+    log_frame(mask, "Green Mask")
     kernel = np.ones((5, 5), np.uint8)
     mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=3)
+    log_frame(mask, "Morphed 1")
     mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel, iterations=1)
+    log_frame(mask, "Morphed 2")
     return mask
 
 
@@ -37,14 +60,20 @@ def on_field_mask(pitchMask):
 
 
 def mask_to_field(frame):
-    return cv.bitwise_and(frame, frame, mask=on_field_mask(pitch_mask(frame)))
+    fieldMask = on_field_mask(pitch_mask(frame))
+    log_frame(fieldMask, "Field Mask")
+    return cv.bitwise_and(frame, frame, mask=fieldMask)
 
 
 def extract_players(frame):
     pitchMask = pitch_mask(frame)
-    frame = cv.bitwise_and(frame, frame, mask=on_field_mask(pitchMask))
-    reversedMask = cv.bitwise_not(pitchMask)
-    players = cv.bitwise_and(frame, frame, mask=reversedMask)
+    onFieldMask = on_field_mask(pitchMask)
+    field = cv.bitwise_and(frame, frame, mask=onFieldMask)
+    log_frame(field, "Field")
+    notPitchMask = cv.bitwise_not(pitchMask)
+    log_frame(notPitchMask, "Not pitch mask")
+    players = cv.bitwise_and(field, field, mask=notPitchMask)
+    log_frame(players, "Players")
     return players
 
 
@@ -64,28 +93,3 @@ def find_lines(frame):
     cv.imshow('frame', frame)
     cv.waitKey(0)
     return frame
-
-
-def interactive_find_pitch(frame):
-    blurred = cv.medianBlur(frame, 3)
-    mask = mask_green(blurred)
-    kernel = np.ones((5, 5), np.uint8)
-    morphed = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=5)
-    morphed = cv.morphologyEx(morphed, cv.MORPH_CLOSE, kernel, iterations=1)
-    contours, hierarchy = cv.findContours(
-            morphed, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    largestContour = max(contours, key=cv.contourArea)
-    blank = np.zeros((frame.shape[0], frame.shape[1]), dtype="uint8")
-    contour_mask = cv.drawContours(
-            blank, [largestContour], -1, (255, 255, 255), -1)
-    masked = cv.bitwise_and(frame, frame, mask=contour_mask)
-    all_frames = [frame, blurred, mask, morphed, contour_mask, masked]
-    idx = 0
-    while (idx < len(all_frames)):
-        cv.imshow('frame', all_frames[idx])
-        key = cv.waitKeyEx(0)
-        if key == 2424832:
-            idx = idx - 1
-        else:
-            idx = idx + 1
-    return masked
