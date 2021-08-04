dccc017974a6e06582495c890ff8b17e9fe5df7f
jyockey <sidereal@gmail.com>
2021-08-03 22:58:48 -0700

    Support transparent saving and loading of video state

--- src/footeye/model/vidinfo.py
+++ src/footeye/model/vidinfo.py
@@ -1,8 +1,32 @@
+from hashlib import blake2b
+import pickle
+import os.path
+
 
 class VidInfo:
     vidFilePath = None
     frameCount = 0
+    fieldColorExtents = None
     scenes = []
 
     def __init__(self, vidFilePath):
         self.vidFilePath = vidFilePath
+
+    def save(self):
+        filename = VidInfo._hashName(self.vidFilePath) + ".vidinfo"
+        with open(filename, 'w') as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def _hashName(filePath):
+        h = blake2b(digest_size=16)
+        h.update(filePath)
+        return h.hexdigest()
+
+    @staticmethod
+    def forPath(vidFilePath):
+        filename = VidInfo._hashName(vidFilePath) + ".vidinfo"
+        if os.path.isfile(filename):
+            with open(filename, 'r') as f:
+                return pickle.load(f)
+        return VidInfo(vidFilePath)
