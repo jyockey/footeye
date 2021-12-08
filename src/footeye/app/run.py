29db801a99c731a28e20509b884378b816539be5
jyockey <sidereal@gmail.com>
2021-12-08 12:50:00 -0800

    Clean out leftover test code

--- src/footeye/app/run.py
+++ src/footeye/app/run.py
@@ -1,7 +1,6 @@
 from footeye.app.model import VidInfo, Project, FrameInfo, MatchData
 
 import argparse
-import cv2 as cv
 import enum
 import pickle
 import scene_breaker
@@ -140,32 +139,3 @@ def run_app():
 
 
 run_app()
-
-
-# variance = np.var(frames, axis=0)
-# flattened = variance.reshape(-1, variance.shape[-1])
-# print(flattened.shape)
-# summed = np.sum(flattened, axis=1)
-# interpolated = np.interp(summed, (summed.min(), summed.max()), (255, 0))
-# greyscale = interpolated.reshape((medianFrame.shape[0], -1)).astype(dtype=np.uint8)
-# features.log_frame(greyscale, "Variance grayscale")
-# _, variance_mask = cv.threshold(greyscale, 220, 255, cv.THRESH_BINARY)
-# features.log_frame(variance_mask, "Variance Mask")
-# masked = cv.bitwise_and(medianFrame, medianFrame, mask=variance_mask)
-# features.log_frame(masked, "Masked")
-
-#frameutils.pixel_hue_variance(frames)
-# frame = frameutils.extract_frame(vid.vidFilePath, 75950)
-# print(vid.frameCount)
-
-# features.extract_players(frame)
-
-
-#pitch = features.interactive_find_pitch(frame)
-#edges = features.find_lines(pitch)
-#cv.imshow('frame', edges)
-#cv.imshow('frame', features.extract_players(frame))
-#cv.waitKey(0)
-
-
-#cv.destroyAllWindows()
